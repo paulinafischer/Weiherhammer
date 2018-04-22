13bb820af6da6e81cd8850437f5a3fe2f2d7fd5f
Pauline <paulinafischer@users.noreply.github.com>
2018-04-22 20:24:21 +0200

    it works :)

--- app/src/main/java/com/example/android/weiherhammer/Places.java
+++ app/src/main/java/com/example/android/weiherhammer/Places.java
@@ -21,23 +21,32 @@ public class Places {
         ));
 
         list.add(new Location(
-                context.getString(R.string.place_fishing_name),
-                context.getString(R.string.place_fishing_description),
-                null,
-                null,
-                context.getString(R.string.place_fishing_schedule),
+                context.getString(R.string.place_bhs_name),
+                context.getString(R.string.place_bhs_description),
+                context.getString(R.string.place_bhs_address),
+                context.getString(R.string.place_bhs_phone),
+                context.getString(R.string.place_bhs_schedule),
                 context.getString(R.string.price),
-                R.drawable.angeln
+                R.drawable.bhs
         ));
 
         list.add(new Location(
-                context.getString(R.string.place_photography_name),
-                context.getString(R.string.place_photography_description),
-                null,
-              null,
-                context.getString(R.string.place_photography_schedule),
+                context.getString(R.string.place_kirche_name),
+                context.getString(R.string.place_kirche_description),
+                context.getString(R.string.place_kirche_address),
+                context.getString(R.string.place_kirche_phone),
+                context.getString(R.string.place_kirche_schedule),
                 context.getString(R.string.price),
-                R.drawable.weiherhammer
+                R.drawable.kirche
+        ));
+        list.add(new Location(
+                context.getString(R.string.place_platz_name),
+                context.getString(R.string.place_platz_description),
+                context.getString(R.string.place_platz_address),
+                context.getString(R.string.place_platz_phone),
+                context.getString(R.string.place_platz_schedule),
+                context.getString(R.string.price),
+                R.drawable.spielplatz
         ));
     }
 }
